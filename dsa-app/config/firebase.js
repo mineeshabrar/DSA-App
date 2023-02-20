d4e85d3dd6b1c6f27661014036027263525bdab1
aabhaschopra <aabhaschopra@gmail.com>
2023-02-20 23:59:36 +0530

    Changes

---
+++ dsa-app/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAK4KkTeOzQEZl-xetDQp7809ON6sJDUd8",
+  authDomain: "dsa-app-e8d34.firebaseapp.com",
+  projectId: "dsa-app-e8d34",
+  storageBucket: "dsa-app-e8d34.appspot.com",
+  messagingSenderId: "444013291259",
+  appId: "1:444013291259:web:3028342483bd3f5124325e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
