f36faa5c2b7f87b5e77d4f652de53668bf3f9b88
aabhaschopra <aabhaschopra@gmail.com>
2023-02-21 00:19:24 +0530

    Added auth component

---
+++ dsa-app/src/components/auth.js
@@ -0,0 +1,10 @@
+import { auth } from "../config/firebase";
+import {  } from "firebase/auth"
+
+export const Auth = () => {
+    return (
+        <div>
+            <button> Google Sign-in </button>
+        </div>
+    );
+};
--- dsa-app/src/config/firebase.js
+++ dsa-app/src/config/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
   apiKey: "AIzaSyAK4KkTeOzQEZl-xetDQp7809ON6sJDUd8",
@@ -10,3 +11,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
