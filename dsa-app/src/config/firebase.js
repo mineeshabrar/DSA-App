9d365c37c1a4b948c959bcbeeacac325c574dde7
aabhaschopra <aabhaschopra@gmail.com>
2023-02-21 00:04:11 +0530

    refactoring

--- dsa-app/src/config/firebase.js
+++ dsa-app/src/config/firebase.js
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAK4KkTeOzQEZl-xetDQp7809ON6sJDUd8",
   authDomain: "dsa-app-e8d34.firebaseapp.com",
@@ -13,5 +9,4 @@ const firebaseConfig = {
   appId: "1:444013291259:web:3028342483bd3f5124325e"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
