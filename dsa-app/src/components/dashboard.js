2b32c4bcbf2187e0372aabc7871a3558f1a05a0d
aabhaschopra <aabhaschopra@gmail.com>
2023-02-26 12:35:36 +0530

    Update dashboard.js

--- dsa-app/src/components/dashboard.js
+++ dsa-app/src/components/dashboard.js
@@ -1,27 +1,10 @@
 import { auth } from "../config/firebase";
-import { app } from "../config/firebase";
-import { getFirestore, doc, getDoc } from "firebase/firestore";
+// import { app } from "../config/firebase";
+// import { getFirestore, doc, getDoc } from "firebase/firestore";
 
 export const Dashboard = () => {
-    const isValidEmail = async () => {
-        try {
-            const  db = getFirestore(app);
-
-            const docRef = doc(db, "students", "");
-            const docSnap = await getDoc(docRef);
-
-            if (docSnap.exists()) {
-            console.log("Document data:", docSnap.data());
-            } else {
-            console.log("No such document!");
-            }
-        } catch (err) {
-            console.error(err);
-        }
-    };
-
-    if (isValidEmail(auth.currentUser.email)) {
-
+    if (auth.currentUser.email.endsWith('pec.edu.in')) {
+        
     }
 
     return (
